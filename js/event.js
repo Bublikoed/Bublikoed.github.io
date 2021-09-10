0741ac618c047b61454f60b36c6172649d657ce1
Vls <bestblodd@gmail.com>
2021-09-10 09:58:49 +0300

    add anim proj

--- js/event.js
+++
@@ -1,9 +0,0 @@
-function displayPopup() {
-    $("#site").addClass('popup-open');
-}
-
-function closePopup(redirectUrl) {
-    $('#site').removeClass('popup-open');
-
-    if (redirectUrl) window.location.href = redirectUrl;
-}
